59c427d05c1751d2fb75cccd5cc01993680595e3
Ildefonso González Sánchez <isanchez.extern@univention.de>
2022-02-12 20:51:38 +0100

    Update application interface

--- tfm/userweb/urls.py
+++ tfm/userweb/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('scan', views.scan, name='scan'),
+    path('reports', views.reports, name='reports')
 ]
--- tfm/userweb/views.py
+++ tfm/userweb/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'index.html', context)
+
+def scan(request):
+    context={}
+    return render(request, 'scan.html', context)
+
+def reports(request):
+    context={}
+    return render(request, 'reports.html', context)
